f242f6414f3d952ce99da1d94a5bbdeb806d05bb
Jolt-rex <leepotter010@gmail.com>
2024-02-18 22:29:08 +1000

    Seeded default admin user with Admin role

--- JoltXServer/Program.cs
+++ JoltXServer/Program.cs
@@ -3,44 +3,78 @@ using JoltXServer.DataAccessLayer;
 using JoltXServer.Models;
 using JoltXServer.Repositories;
 using JoltXServer.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-builder.Services.AddSingleton<IDatabaseSqlite>(new DatabaseSqlite());
-builder.Services.AddSingleton<ISymbolRepository, SymbolRepository>();
-builder.Services.AddSingleton<ICandleRepository, CandleRepository>();
-builder.Services.AddSingleton<IExternalAPIService, BinanceService>();
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddDbContext<JoltXServer.DataAccessLayer.UserContext>(options => {
-  options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.AddAuthorization();
-
-builder.Services.AddIdentityApiEndpoints<User>()
-  .AddEntityFrameworkStores<JoltXServer.DataAccessLayer.UserContext>();
-
-var app = builder.Build();
-
-app.Services.GetService<IExternalAPIService>();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
+    private static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        builder.Services.AddSingleton<IDatabaseSqlite>(new DatabaseSqlite());
+        builder.Services.AddSingleton<ISymbolRepository, SymbolRepository>();
+        builder.Services.AddSingleton<ICandleRepository, CandleRepository>();
+        builder.Services.AddSingleton<IExternalAPIService, BinanceService>();
+        builder.Services.AddEndpointsApiExplorer();
+
+        builder.Services.AddDbContext<UserContext>(options =>
+        {
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        builder.Services.AddAuthentication();
+
+        builder.Services.AddAuthorization();
+
+        builder.Services.AddIdentityApiEndpoints<User>()
+          .AddRoles<IdentityRole>()
+          .AddEntityFrameworkStores<UserContext>();
+
+        var app = builder.Build();
+
+        app.Services.GetService<IExternalAPIService>();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+
+        }
+
+        app.MapGroup("api/v1/auth")
+          .MapIdentityApi<User>();
+
+        // app.UseHttpsRedirection();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        // Add default admin account if not exists
+        using (var scope = app.Services.CreateScope())
+        {
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            string email = "admin@admin.com";
+            string password = "Password1!";
+
+            if (await userManager.FindByEmailAsync(email) == null)
+            {
+                var user = new IdentityUser
+                {
+                    UserName = email,
+                    Email = email
+                };
+
+                await userManager.CreateAsync(user, password);
+
+                await userManager.AddToRoleAsync(user, "Admin");
+            }
+        }
+
+        app.Run();
+    }
 }
-
-app.MapIdentityApi<User>();
-
-// app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
